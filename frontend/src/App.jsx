f1e6676d6a8932f728927cc3e3ed9bf806f8e48a
Prathmesh Gawande <gm24ecm3s05@student.nitw.ac.in>
2026-06-07 10:54:20 +0530

    frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-const API = "https://inventory-order-management-system-iu6l.onrender.com";
+const API = "http://127.0.0.1:8000";
+``
 
 function App() {
   const [products, setProducts] = useState([]);
