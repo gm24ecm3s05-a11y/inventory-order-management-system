aa541bac7cade70f181c361ddaf1797a32a6cd7f
Prathmesh Gawande <gm24ecm3s05@student.nitw.ac.in>
2026-06-07 08:34:16 +0530

    Use sqlite for deployment

--- backend/database.py
+++ backend/database.py
@@ -1,13 +1,12 @@
-import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = os.getenv(
-    "DATABASE_URL",
-    "postgresql://postgres:postgres@localhost:5432/inventorydb"
+DATABASE_URL = "sqlite:///./inventory.db"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
 )
 
-engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
-
 Base = declarative_base()
